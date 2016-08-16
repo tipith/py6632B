e474596deca00cb9fa542b8e13340d1b9a006972
Timo Pihlström <timo.pihlstrom@iki.fi>
2016-08-17 00:06:14 +0300

    fix

--- lab_script.py
+++ lab_script.py
@@ -1,29 +1,43 @@
 #!/usr/bin/env python
-
+import sys
 import time
 import logging
 import py6632B
 
+
+# Set the requested battery parameters here 
+#  - EOCV = end of charge voltage
+#  - EODV = end of discharge voltage
+#  - capacity = suspected battery capacity
+batteries = {
+    'Generic Panasonic 18650': {'EOCV': 4.18, 'EODV': 3.0, 'capacity': 5000},
+    'A123 Systems LiFePo4 25560': {'EOCV': 2*3.58, 'EODV': 2*2.0, 'capacity': 2500},
+    'Samsung Mini S3': {'EOCV': 4.18, 'EODV': 3.1, 'capacity': 1600},
+    'Solar Blaster': {'EOCV': 2*4.18, 'EODV': 2*3.1, 'capacity': 8000}
+}
+
+
+def setup_logging():
+    format = logging.Formatter('%(asctime)s %(name)10s: %(message)s', datefmt="%Y-%m-%d %H:%M:%S")
+
+    rootlog = logging.getLogger()
+    rootlog.setLevel(logging.DEBUG)
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setFormatter(format)
+    rootlog.addHandler(ch)
+
+    fh = logging.FileHandler('log.txt')
+    fh.setFormatter(format)
+    rootlog.addHandler(fh)
+
+
 if __name__ == '__main__':
-    logging.basicConfig(filename='log.txt',
-                        format='%(asctime)s %(name)10s: %(message)s',
-                        datefmt="%Y-%m-%d %H:%M:%S",
-                        level=logging.DEBUG)
-    
+    setup_logging()
+
     pwr = py6632B.HP6632B('/dev/ttyUSB0', 1, True)
     pwr.daemon = True
     pwr.start()
-    
-    # Set the requested battery parameters here 
-    #  - EOCV = end of charge voltage
-    #  - EODV = end of discharge voltage
-    #  - capacity = suspected battery capacity
-    batteries = {
-        'Generic Panasonic 18650': {'EOCV': 4.18, 'EODV': 3.0, 'capacity': 5000},
-        'A123 Systems LiFePo4 25560': {'EOCV': 2*3.58, 'EODV': 2*2.0, 'capacity': 2500},
-        'Samsung Mini S3': {'EOCV': 4.18, 'EODV': 3.1, 'capacity': 1600},
-        'Solar Blaster': {'EOCV': 2*4.18, 'EODV': 2*3.1, 'capacity': 8000}
-    }
 
     selected_battery = batteries['Solar Blaster']
     
